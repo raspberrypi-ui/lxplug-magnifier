70f7f0bac92aff5e887b8c77b9064985cad5f8b9
Simon Long <simon@raspberrypi.org>
2020-03-26 23:07:32 +0000

    Simplify (and correct...) loading macro

--- plugins/magnifier/magnifier.c
+++ plugins/magnifier/magnifier.c
@@ -187,7 +187,7 @@ static void mag_destructor (gpointer user_data)
 }
 
 
-#define READ_VAL(name,var,low,high,def) if (config_setting_lookup_int (settings, name, &val)) { if (val >= low || val <= high) var = val; else var = def; } else var = def;
+#define READ_VAL(name,var,low,high,def) if (config_setting_lookup_int (settings, name, &val) && val >= low && val <= high) var = val; else var = def;
 
 /* Plugin constructor. */
 static GtkWidget *mag_constructor (LXPanel *panel, config_setting_t *settings)
@@ -203,7 +203,7 @@ static GtkWidget *mag_constructor (LXPanel *panel, config_setting_t *settings)
     textdomain (GETTEXT_PACKAGE);
 #endif
 
-    READ_VAL ("Shape", mag->shape, -1, 0, 0);
+    READ_VAL ("Shape", mag->shape, 0, 1, 0);
     READ_VAL ("Zoom", mag->zoom, 2, 16, 2);
     READ_VAL ("Width", mag->width, 100, 600, 350);
     READ_VAL ("Height", mag->height, 100, 600, 350);
